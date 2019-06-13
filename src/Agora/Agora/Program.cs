d9d445e5cbe99691bd407ab35235ad30af5cd152
malcolm.mcneely.86 <malcolm.mcneely.86@gmail.com>
2019-06-13 22:01:35 +0100

    Recreated solutions and ASP.NET 2.2 Core app with React and Redux

--- src/Agora/Agora/Program.cs
+++ src/Agora/Agora/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,13 +8,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Agora.Server.ServerHost
+namespace Agora
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
